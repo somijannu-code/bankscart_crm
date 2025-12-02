970e659f22a14bc3ebf5fcaf54086de62a5d4b95
gauravhotstar <hotstaer5@gmail.com>
2025-12-03 00:05:42 +0530

    Update pwa-components.tsx

--- components/pwa-components.tsx
+++ components/pwa-components.tsx
@@ -7,7 +7,7 @@ import { AppStatusBar } from "@/components/pwa/app-status-bar"
 import { NativeInteractions } from "@/components/pwa/native-interactions"
 import { AppShortcuts } from "@/components/pwa/app-shortcuts"
 import { OfflineIndicator } from "@/components/offline-indicator"
-import { CallLogPermission } from "@/components/device/call-log-permission"
+
 import { useEffect } from "react"
 import { toast } from "sonner"
 
@@ -27,21 +27,7 @@ export default function PWAComponents() {
     }
   }, [])
 
-  const handlePermissionGranted = () => {
-    console.log("Call log permission granted")
-    // Here you could initialize call log tracking
-    toast.success("Call log access enabled!", {
-      description: "Your calls will now be tracked automatically when possible"
-    })
-  }
 
-  const handlePermissionDenied = () => {
-    console.log("Call log permission denied")
-    // Here you could show alternative options
-    toast.info("Manual call logging", {
-      description: "You can still log calls manually from lead details"
-    })
-  }
 
   return (
     <>
@@ -49,10 +35,7 @@ export default function PWAComponents() {
       <NativeInteractions />
       <AppShortcuts />
       <NotificationPermission />
-      <CallLogPermission 
-        onPermissionGranted={handlePermissionGranted}
-        onPermissionDenied={handlePermissionDenied}
-      />
+
       <PWAInstallPrompt />
       <PWAUpdatePrompt />
       <OfflineIndicator />
