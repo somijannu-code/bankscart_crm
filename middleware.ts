69817641eaf9a6570f220d4305fb35d7c71dd2b6
gauravhotstar <hotstaer5@gmail.com>
2025-12-23 13:00:38 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -8,13 +8,12 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match all request paths except:
+     * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
+     * - Any file with a common extension (css, js, images, fonts, etc.)
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js|woff|woff2|ttf|eot|pdf|csv|xlsx|json|map)$).*)",
   ],
 }
