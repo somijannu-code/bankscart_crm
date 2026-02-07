9ffab9aa9adc0b9226231d5e0ef99b4d283fe5cc
somijannu-code <somijannu@gmail.com>
2026-02-07 18:02:17 +0530

    Change 'Disbursed' to 'DISBURSED' in select options

--- components/telecaller-lead-filters.tsx
+++ components/telecaller-lead-filters.tsx
@@ -126,7 +126,7 @@ export function TelecallerLeadFilters({ initialSearchParams }: { initialSearchPa
               <SelectItem value="Interested">Interested</SelectItem>
               <SelectItem value="Documents_Sent">Docs Sent</SelectItem>
               <SelectItem value="Login">Login</SelectItem>
-              <SelectItem value="Disbursed">Disbursed</SelectItem>
+              <SelectItem value="DISBURSED">Disbursed</SelectItem>
               <SelectItem value="follow_up">Call Back</SelectItem>
               <SelectItem value="nr">Not Reachable</SelectItem>
               <SelectItem value="not_eligible">Not Eligible</SelectItem>
